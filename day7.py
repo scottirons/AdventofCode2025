14bcbb5bd305be6578285290195495495ebf76d4
scottpirons@gmail.com <87613441+scottirons@users.noreply.github.com>
2025-12-07 10:24:13 -0700

    day 7

--- day7.py
+++ day7.py
@@ -1,9 +1,10 @@
-from collections import deque
+from collections import deque, defaultdict
+from heapq import heappush, heappop
 
 source = "day7.txt"
 #source = "sample.txt"
 
-# part a
+# part 1
 with open(source, 'r') as f:
     data = f.read().split('\n')
 
@@ -11,8 +12,10 @@ a_splits = set()
 visited = set()
 queue = deque()
 
-queue.append((0, data[0].index('S')))
-visited.add((0, data[0].index('S')))
+start_col = data[0].index('S')
+
+queue.append((0, start_col))
+visited.add((0, start_col))
 
 while queue:
     row, col = queue.popleft()
@@ -28,7 +31,27 @@ while queue:
 
 print(f"Part 1: {len(a_splits)}")
 
-# part b
+# part 2
 # memo should be a dict instead of a set where I keep track of the running total of paths that led to the point.
 # so if coords in memo, increment amount. then carry on that amount to the next split and increment/set the memos of the
 # L/R points to the carried on value
+heap = [(0, start_col)]
+visited_count = defaultdict(int, {(0, start_col): 1})
+b_result = 0
+
+while heap:
+    row, col = heappop(heap)
+    curr_count = visited_count[(row, col)]
+    next_row = row + 1
+    while next_row < len(data):
+        if data[next_row][col] == '^':
+            heappush(heap, (next_row, col - 1)) if (next_row, col - 1) not in visited_count else None
+            heappush(heap, (next_row, col + 1)) if (next_row, col + 1) not in visited_count else None
+            visited_count[(next_row, col - 1)] += curr_count
+            visited_count[(next_row, col + 1)] += curr_count
+            break
+        next_row += 1
+    if next_row == len(data):
+        b_result += curr_count
+
+print(f"Part 2: {b_result}")
